f750c4c51927d7cbef313657351c6342188506b2
Ousseynou Dione <ousseynou.dione.ext@cihbank.ma>
2025-03-20 20:50:14 +0000

    The first commit add data transfroamtion

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -1,12 +1,11 @@
 from dataclasses import dataclass
 
-@dataclass()
+@dataclass
 class DataIngestionArtifact:
     trained_file_path:str
     test_file_path:str
 
-
-@dataclass() # -> décorateur
+@dataclass
 class DataValidationArtifact:
     validation_status: bool
     valid_train_file_path: str
@@ -14,3 +13,21 @@ class DataValidationArtifact:
     invalid_train_file_path: str
     invalid_test_file_path: str
     drift_report_file_path: str
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+
+@dataclass
+class ClassificationMetricArtifact:
+    f1_score: float
+    precision_score: float
+    recall_score: float
+    
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path: str
+    train_metric_artifact: ClassificationMetricArtifact
+    test_metric_artifact: ClassificationMetricArtifact
